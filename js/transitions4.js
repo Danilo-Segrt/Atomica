0864d0831314b92ea93fe0ed9e826f356c86a6de
Danilo-Segrt <danilo.segrt6@gmail.com>
2025-04-06 22:34:17 +0200

    kraj

---
+++ js/transitions4.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function triggerFlashTransition(nextPage) {
+        const flashOverlay = document.createElement('div');
+        flashOverlay.className = 'flash-overlay';
+        document.body.appendChild(flashOverlay);
+        
+        setTimeout(() => {
+            window.location.href = nextPage;
+        }, 500);
+    }
+
+    const nextPageBtn = document.getElementById('nextPageBtn');
+    if (nextPageBtn) {
+        nextPageBtn.addEventListener('click', function(e) {
+            e.preventDefault();
+            triggerFlashTransition('siriusnivo3.html');
+        });
+    }
+
+    const transitionBtn = document.getElementById('transition-btn');
+    if (transitionBtn) {
+        transitionBtn.addEventListener('click', function(e) {
+            e.preventDefault();
+            const nextPage = this.getAttribute('data-next-page');
+            if (nextPage) {
+                triggerFlashTransition(nextPage);
+            }
+        });
+    }
+});
